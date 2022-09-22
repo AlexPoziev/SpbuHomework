d2b3ffa49635c2ce3065451c35670e035a067c23
Aleksey Poziev <poziev24@gmail.com>
2022-09-22 20:29:38 +0300

    added insertionSort function

--- Homework3/smartQsort.c
+++ Homework3/smartQsort.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 void swap(int *first, int *second) {
     int temp = *first;
     *first = *second;
     *second = temp;
+}
+
+void insertionSort(int array[], int low, int high) {
+    for (int i = low; i <= high; ++i) {
+        int j = i;
+        while (j > low && array[j - 1] > array[j]) {
+            swap(&array[j - 1] , &array[j]);
+            --j;
+        }
+    }
 }
 
 int main () {
