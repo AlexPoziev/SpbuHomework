baea9708758488c2a4b30b2c3cd4ee7fb4b1734c
Aleksey Poziev <poziev24@gmail.com>
2022-09-22 20:09:20 +0300

    Started program dev and added swap function

---
+++ Homework3/smartQsort.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void swap(int *first, int *second) {
+    int temp = *first;
+    *first = *second;
+    *second = temp;
+}
+
+int main () {
+
+    return 0;
+}
