14179952d17db6b944b1f3fb492bc09a6a4a188a
Aleksey Poziev <poziev24@gmail.com>
2022-09-23 20:43:24 +0300

    added mostFrequent function
    it's search most frequent element in sort array

--- Homework3/mostFrequentElement.c
+++ Homework3/mostFrequentElement.c
@@ -9,7 +9,29 @@ int compare(const void *x_void, const void *y_void) {
     return x - y;
 }
 
-int main() {
+int mostFrequent(int array[], int arrayLenght) {
+    unsigned int maxRow = 0;
+    unsigned int recentCount = 1;
+    int maxRowElement = 0;
 
+    for (int i = 1; i < arrayLenght; ++i) {
+        if (array[i] == array[i - 1]) {
+            ++recentCount;
+        } else {
+            if (maxRow < recentCount) {
+                maxRowElement = array[i - 1];
+                maxRow = recentCount;
+            }
+            recentCount = 1;
+        }
+    }
+    
+    return recentCount > maxRow ? array[arrayLenght - 1] : maxRowElement;
+}
+
+int main() {
+    int array[8] = {1,1,2,2,2,3,3,3};
+    int check = mostFrequent(array, 8);
+    printf("%d", check);
     return 0;
 }
