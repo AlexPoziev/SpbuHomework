8f8dc0a8e506f0908364a3900f05072e547b8bc2
Aleksey Poziev <poziev24@gmail.com>
2022-09-23 21:16:55 +0300

    added qsort into mostFrequent function

--- Homework3/mostFrequentElement.c
+++ Homework3/mostFrequentElement.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 int compare(const void *x_void, const void *y_void) {
     int x = *(int *)x_void;
@@ -9,29 +10,34 @@ int compare(const void *x_void, const void *y_void) {
     return x - y;
 }
 
-int mostFrequent(int array[], int arrayLenght) {
+int mostFrequent(int array[], int arrayLength) {
     unsigned int maxRow = 0;
     unsigned int recentCount = 1;
     int maxRowElement = 0;
+    
+    //создание копии массива, так как функция не всегда должна менять проверяемый массив
+    int *sortedArray =  (int *)(calloc(arrayLength, sizeof(int)));
+    memcpy(sortedArray, array, sizeof(int) * arrayLength);
+    qsort(sortedArray, arrayLength, sizeof(int), compare);
 
-    for (int i = 1; i < arrayLenght; ++i) {
-        if (array[i] == array[i - 1]) {
+    for (int i = 1; i < arrayLength; ++i) {
+        if (sortedArray[i] == sortedArray[i - 1]) {
             ++recentCount;
         } else {
             if (maxRow < recentCount) {
-                maxRowElement = array[i - 1];
+                maxRowElement = sortedArray[i - 1];
                 maxRow = recentCount;
             }
             recentCount = 1;
         }
     }
     
-    return recentCount > maxRow ? array[arrayLenght - 1] : maxRowElement;
+    return recentCount > maxRow ? sortedArray[arrayLength - 1] : maxRowElement;
 }
 
 int main() {
-    int array[8] = {1,1,2,2,2,3,3,3};
-    int check = mostFrequent(array, 8);
+    int array[5] = {5, 2, 3, 1, 3};
+    int check = mostFrequent(array, 5);
     printf("%d", check);
     return 0;
 }
