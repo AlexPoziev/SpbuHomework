ce5e83e23610db7a86b29c39f87d6d807065f1d2
Aleksey Poziev <poziev24@gmail.com>
2022-09-24 10:39:00 +0300

    add qsort

--- Homework3/search.c
+++ Homework3/search.c
@@ -14,7 +14,40 @@ void randomArrayFilling(unsigned int arraySize, int array[]) {
     }
 }
 
+int semiQsort(int low, int high, int array[]) {
+    const int firstNumber = array[low];
+    int border = high;
+
+    for (int i = low + 1; i < border; ++i) {
+        if (array[i] >= firstNumber) {
+            while (array[border] >= firstNumber && i < border) {
+                --border;
+            }
+            swap(&array[i], &array[border]);
+            --border;
+        }
+    }
+
+    if (firstNumber < array[border]) {
+        --border;
+    }
+
+    if (!(low == high)) {
+        swap(&array[low], &array[border]);;
+    }
+
+    return border;
+}
+
+void qsortRecursion(int low, int high, int array[]) {
+    if (low < high) {
+            int border = semiQsort(low, high, array);
+            qsortRecursion(low, border - 1, array);
+            qsortRecursion(border + 1, high, array);
+    }
+}
+
 int main() {
-    
+
     return 0;
 }
