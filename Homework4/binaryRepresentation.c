cd71424a6f7255addb9892b5d1000e1cf63a6efc
Aleksey Poziev <poziev24@gmail.com>
2022-09-28 23:46:51 +0300

    decimalRepresentation add

--- Homework4/binaryRepresentation.c
+++ Homework4/binaryRepresentation.c
@@ -12,13 +12,29 @@ void binaryRepresentation(int number, int numberDigits, short* representation) {
     }
 } 
 
+int decimalRepresentation(int numberDigits, short *binary) {
+    int decimal = 0;
+    int current = 1;
+
+    for (int i = 0; i < numberDigits - 1; ++i) {
+        decimal += binary[numberDigits - i - 1] * current;
+        current <<= 1;
+    }
+
+    decimal -= binary[0] * current;
+
+    return decimal;
+}
+
 int main() {
-    int x = -5;
+    int x = 5;
     short *check = (short*)(calloc(4, sizeof(short)));
     binaryRepresentation(x, 4, check);
-    for(int i = 0; i < 4; ++i) {
+   /* for(int i = 0; i < 4; ++i) {
         printf("%d", check[i]);
-    }
+    }*/
+    int checkint = decimalRepresentation(4, check);
+    printf("%d", checkint);
 
     return 0;
 }
