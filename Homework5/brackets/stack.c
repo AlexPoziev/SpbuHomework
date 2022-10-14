aee0da681c686dbb69dc2773b01afa32e5c49d2c
Aleksey Poziev <poziev24@gmail.com>
2022-10-14 17:36:48 +0300

    Update stack.c
    add realisation of stack.h module

--- Homework5/brackets/stack.c
+++ Homework5/brackets/stack.c
@@ -2,3 +2,47 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+int push(Stack **head, int value) {
+    Stack *temp = (Stack*)(malloc(sizeof(Stack)));
+    if (temp == NULL) {
+        return 1;
+    }
+
+    temp->value = value;
+    temp->next = *head;
+    *head = temp;
+
+    return 0;
+}
+
+int pop(Stack **head, int *errorCode) {
+    if (*head == NULL) {
+        if (errorCode != NULL) {
+            *errorCode = -1;
+        }
+
+        return 0;
+    }
+    if (errorCode != NULL) {
+        *errorCode = 0;
+    }
+
+    int value = (*head)->value;
+    Stack *temp = (*head)->next;
+    free(*head);
+    *head = temp;
+
+    return value;
+}
+
+bool isEmpty(Stack *head) {
+    return (head == NULL) ? true : false;
+}
+
+void clearStack(Stack **head) {
+    Stack *next= NULL;
+    while (!isEmpty(*head)) {
+        int errorCode = 0;
+        pop(head, &errorCode);
+    }
+}
