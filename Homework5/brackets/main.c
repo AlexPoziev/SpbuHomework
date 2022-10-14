081dc9c0fcc95fe1b705731bdd894a0f79e2e393
Aleksey Poziev <poziev24@gmail.com>
2022-10-14 18:51:04 +0300

    add correctTest and incorrectTest functions

--- Homework5/brackets/main.c
+++ Homework5/brackets/main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-bool bracketsBalance(char *string, int size) {
+bool bracketsBalance(char *string, unsigned long size) {
     if (size == 0) {
         return true;
     }
@@ -31,11 +31,33 @@ bool bracketsBalance(char *string, int size) {
     }
 }
 
-int main() {
-    char string[100];
+bool correctTest(void) {
+    char firstCheck[6] = "()[]{}";
+    char secondCheck[0] = "";
+    char thirdCheck[16] = "([{}]){}{}[]{()}";
 
-    printf("Input a string of brackets balance: ");
+    return bracketsBalance(firstCheck, 6) && bracketsBalance(secondCheck, 0) && bracketsBalance(thirdCheck, 16);
+}
+
+bool incorrectTest(void) {
+    char firstCheck[1] = "(";
+    char secondCheck[3] = "())";
+    char thirdCheck[2] = "(}";
+    char fourthCheck[4] = "{(})";
+
+    return !bracketsBalance(firstCheck, 1) && !bracketsBalance(secondCheck, 3) && !bracketsBalance(thirdCheck, 2) && !bracketsBalance(fourthCheck, 4);
+}
+
+int main() {
+    if (!correctTest() || !incorrectTest()) {
+        printf("Tests failed");
+        return 1;
+    }
+
+    char string[1000];
+
+    printf("Input a string of brackets balance with length below 1000: ");
     fgets(string, sizeof string, stdin);
-    printf("Balance of brackets: %s \n", (bracketsBalance(string, strlen(string) - 1)) ? "correct" : "incorrect");
+    printf("Balance of brackets: %s", (bracketsBalance(string, strlen(string) - 1)) ? "correct" : "incorrect");
     return 0;
 }
