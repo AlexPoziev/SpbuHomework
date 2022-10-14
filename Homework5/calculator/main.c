5b5181002745033dee7935b6585e0d9547740c1a
Aleksey Poziev <poziev24@gmail.com>
2022-10-14 23:29:37 +0300

    add user input

--- Homework5/calculator/main.c
+++ Homework5/calculator/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 double performOperation(double firstNumber, double secondNumber, char operation, int *errorCode) {
     switch (operation) {
@@ -34,6 +35,9 @@ double postfixCalculator(char *expression, unsigned expressionSize, int *errorCo
     *errorCode = 0;
 
     for (int i = 0; i < expressionSize; ++i) {
+        if (expression[i] == ' ') {
+            continue;
+        }
         if (expression[i] >= '0' && expression[i] <= '9') {
             *errorCode = push(&head, expression[i] - '0');
             //check for memory in push
@@ -42,6 +46,7 @@ double postfixCalculator(char *expression, unsigned expressionSize, int *errorCo
             }
         } else {
             double firstNumber = pop(&head, errorCode);
+            //check that stack has enough numbers for completing operation
             if (*errorCode == -1) {
                 //code 2 incorrect expression
                 printf("1");
@@ -51,18 +56,22 @@ double postfixCalculator(char *expression, unsigned expressionSize, int *errorCo
             if (*errorCode == -1) {
                 return 0;
             }
+            //get a result of operation
             firstNumber = performOperation(firstNumber, secondNumber, expression[i], errorCode);
             if (*errorCode == -1) {
                 return 0;
             }
+            //push result to stack
             push(&head, firstNumber);
         }
     }
+
     double sum = pop(&head, errorCode);
-    //check that its only number in stack
+    //check that it's only number in stack
     if (isEmpty(head)) {
         return sum;
     } else {
+        //clearStack if stack has extra values
         clearStack(&head);
         *errorCode = -1;
         return 0;
@@ -70,14 +79,21 @@ double postfixCalculator(char *expression, unsigned expressionSize, int *errorCo
 }
 
 int main() {
-    char *test = "+";
+    char expression[1000] = {0};
+
+    printf("Input postfix expression with length below 1000: ");
+    fgets(expression, sizeof(expression), stdin);
     int errorCode = 0;
-    double check = postfixCalculator(test, 8, &errorCode);
+    double result = postfixCalculator(expression, strlen(expression) - 1, &errorCode);
     if (errorCode == -1) {
-        printf("incorrect expression");
+        printf("Incorrect expression");
+        return 0;
     }
     if (errorCode == 1) {
         printf("Not enough memory");
+        return 1;
     }
+    printf("Result of expression: %g", result);
+
     return 0;
 }
