258254c21cd543d0781b598c7a7943bb5f443650
Aleksey Poziev <poziev24@gmail.com>
2022-10-15 15:22:36 +0300

    add int stack module

---
+++ Homework5/stack/stack.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <stdbool.h>
+
+typedef struct Stack {
+    int value;
+    struct Stack *next;
+}Stack;
+
+//add value to stack
+int push(Stack **head, int value);
+
+//delete value from stack
+int pop(Stack **head, int *errorCode);
+
+//clear memory
+void clearStack(Stack **head);
+
+//check for empty head
+bool isEmpty(Stack *head);
