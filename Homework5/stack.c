d97d32a71371ac42626da5b774584894a69b9c21
Aleksey Poziev <poziev24@gmail.com>
2022-10-15 17:16:03 +0300

    create project

---
+++ Homework5/stack.c
@@ -0,0 +1,7 @@
+#include "stack.h"
+
+#include <stdio.h>
+
+void hello(void) {
+    printf("Hello, World!\n");
+}
---
+++ Homework5/stack.h
@@ -0,0 +1,6 @@
+#ifndef HOMEWORK5_STACK_H
+#define HOMEWORK5_STACK_H
+
+void hello(void);
+
+#endif //HOMEWORK5_STACK_H
