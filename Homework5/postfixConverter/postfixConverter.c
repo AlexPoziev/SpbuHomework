bd383c8572b5b34d3966b04116e753e6f1004cb5
Aleksey Poziev <poziev24@gmail.com>
2022-10-15 22:38:38 +0300

    add function priorityDeterminant

--- Homework5/postfixConverter/postfixConverter.c
+++ Homework5/postfixConverter/postfixConverter.c
@@ -1,4 +1,21 @@
 #include "postfixConverter.h"
 #include "../stack/stack.h"
 
-
+int priorityDeterminant(char operation) {
+    switch (operation) {
+        case '+':
+        case '-':
+        {
+            return 0;
+        }
+        case '*':
+        case '/':
+        {
+            return 1;
+        }
+        default:
+        {
+            return -1;
+        }
+    }
+}
