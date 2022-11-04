088ddb60ccecd33c9d88a1c66a3aa3989eb25302
Aleksey Poziev <poziev24@gmail.com>
2022-11-04 21:47:59 +0300

    refactor: change struct, solve with height

--- Homework8/AVLTree.c
+++ Homework8/AVLTree.c
@@ -6,23 +6,23 @@
 typedef struct Node {
     char *value;
     int token;
-    int balance;
+    int height;
     struct Node *leftChild;
     struct Node *rightChild;
 } Node;
 
 typedef struct Dictionary {
-    Node *head;
-};
+    Node *dictionary;
+} Dictionary;
 
 typedef enum Direction{
-    left,
-    right
+    left = -1,
+    right = 1
 } Direction;
 
 Dictionary* createDictionary(void) {
     Dictionary *newDictionary = malloc(sizeof(Dictionary));
-    newDictionary->head = NULL;
+    newDictionary->dictionary = NULL;
     return newDictionary;
 }
 
@@ -39,6 +39,20 @@ Node* createNode(int token, char *value) {
     return newNode;
 }
 
+int getHeight(Node *node) {
+    if (node == NULL) {
+        return 0;
+    }
+    return node->height;
+}
+
+int getBalance(Node *node) {
+    if (node == NULL) {
+        return 0;
+    }
+    return getHeight(node->rightChild) - getHeight(node->leftChild);
+}
+
 Node* rotateLeft(Node *currentNode) {
     Node *rightChild = currentNode->rightChild;
     Node *leftGrandson = rightChild->leftChild;
@@ -46,10 +60,13 @@ Node* rotateLeft(Node *currentNode) {
     rightChild->leftChild = currentNode;
     currentNode->rightChild = leftGrandson;
 
+    currentNode->balance = getBalance(currentNode->rightChild) - getBalance(currentNode->leftChild);
+    rightChild->balance = getBalance(rightChild->rightChild) - getBalance(rightChild->leftChild);
+
     return rightChild;
 }
 
-Node* rotateBigLeft(Node* currentNode) {
+Node* bigRotateLeft(Node* currentNode) {
     Node *rightChild = currentNode->rightChild;
     Node *leftGrandson = rightChild->leftChild;
     Node *leftGrandGrandson = leftGrandson->leftChild;
@@ -59,6 +76,10 @@ Node* rotateBigLeft(Node* currentNode) {
     leftGrandson->rightChild = rightChild;
     currentNode->rightChild = leftGrandGrandson;
     rightChild->leftChild = rightGrandGrandson;
+
+    currentNode->balance = getBalance(currentNode->rightChild) - getBalance(currentNode->leftChild);
+    rightChild->balance = getBalance(rightChild->rightChild) - getBalance(rightChild->leftChild);
+    leftGrandson->balance = getBalance(leftGrandson->rightChild) - getBalance(leftGrandson->leftChild);
 
     return leftGrandson;
 }
@@ -70,10 +91,13 @@ Node* rotateRight(Node *currentNode) {
     leftChild->rightChild = currentNode;
     currentNode->leftChild = rightGrandson;
 
+    currentNode->balance = getBalance(currentNode->rightChild) - getBalance(currentNode->leftChild);
+    leftChild->balance = getBalance(leftChild->rightChild) - getBalance(leftChild->leftChild);
+
     return leftChild;
 }
 
-Node* rotateBigRight(Node* currentNode) {
+Node* bigRotateRight(Node* currentNode) {
     Node *leftChild = currentNode->leftChild;
     Node *rightGrandson = leftChild->rightChild;
     Node *rightGrandGrandson = rightGrandson->rightChild;
@@ -84,5 +108,72 @@ Node* rotateBigRight(Node* currentNode) {
     currentNode->leftChild = rightGrandGrandson;
     leftChild->rightChild = leftGrandGrandson;
 
+    currentNode->balance = getBalance(currentNode->rightChild) - getBalance(currentNode->leftChild);
+    leftChild->balance = getBalance(leftChild->rightChild) - getBalance(leftChild->leftChild);
+    rightGrandson->balance = getBalance(rightGrandson->rightChild) - getBalance(rightGrandson->leftChild);
+
     return rightGrandson;
+}
+
+Node* balance(Node *node) {
+    if (node->balance == 2) {
+        if (node->rightChild->balance >= 0) {
+            return rotateLeft(node);
+        }
+        return bigRotateLeft(node);
+    }
+    if (node->balance == -2) {
+        if (node->leftChild->balance <= 0){
+            return rotateRight(node);
+        }
+        return bigRotateRight(node);
+    }
+
+    return node;
+}
+
+Node* insert(Node *node, int token, char *value, bool *isPart, int *heightDifference) {
+    if (node == NULL) {
+        Node *newNode = createNode(token, value);
+        return newNode;
+    }
+
+    Direction direction = 0;
+    if (token < node->token) {
+        node->leftChild = insert(node->leftChild, token, value, isPart, heightDifference);
+        direction = left;
+    } else if (token > node->token) {
+        node->rightChild = insert(node->rightChild, token, value, isPart, heightDifference);
+        direction = right;
+    } else {
+        *isPart = true;
+        char *newValue = calloc(strlen(value), sizeof(char));
+        strcpy(newValue, value);
+        free(node->value);
+        node->value = newValue;
+        return node;
+    }
+
+    if (*isPart) {
+        return node;
+    }
+    node->balance += direction;
+    return balance(node);
+}
+
+int addValue(Dictionary *dictionary, int token, char* value) {
+    if (dictionary == NULL) {
+        return 1;
+    }
+    if (dictionary->dictionary == NULL) {
+        Node *newValue = createNode(token, value);
+        dictionary->dictionary = newValue;
+
+        return 0;
+    }
+
+    bool isPart = false;
+    dictionary->dictionary = insert(dictionary->dictionary, token, value, &isPart);
+
+    return 0;
 }
--- Homework8/main.c
+++ Homework8/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include "AVLTree.h"
 
 int main() {
-    printf("Hello, World!\n");
+    Dictionary *dictionary = createDictionary();
+    addValue(dictionary, 1, "gg");
+    addValue(dictionary, 22, "check");
+    addValue(dictionary, 53, "ttet");
+    addValue(dictionary, 81, "tetetet");
+    addValue(dictionary, 94, "tetet");
+    addValue(dictionary, 37, "tet");
+    addValue(dictionary, 95, "tet");
+
+
+
     return 0;
 }
