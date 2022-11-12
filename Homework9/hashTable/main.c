c6039f6bf931900b5d528e0b49de09bdff8f3963
Aleksey Poziev <poziev24@gmail.com>
2022-11-13 01:53:57 +0300

    feat: add main

--- Homework9/hashTable/main.c
+++ Homework9/hashTable/main.c
@@ -8,16 +8,47 @@ int main() {
         printf("Tests Failed");
         return 1;
     }
+    printf("Print approximate number of words, larger than zero: ");
+    unsigned int tableSize = 0;
+    int errorCode = scanf("%u", &tableSize);
+    while (errorCode <= 0) {
+        fflush(stdin);
+        printf("it's string, please repeat input: ");
+        errorCode = scanf("%u", &tableSize);
+    }
 
-    HashTable *table = createHashTable(100);
-    getFromFile("testSecond.txt", table);
+    HashTable *table = createHashTable((unsigned int)(tableSize * 1.3));
+    if (table == NULL) {
+        printf("Not enough memory");
+        return 1;
+    }
+
+    printf("Print name of file, with length less than 20: ");
+    char fileName[20] = {0};
+    scanf("%s", fileName);
+
+    errorCode = getFromFile(fileName, table);
+    if (errorCode == -1) {
+        printf("No file with this name");
+        return 0;
+    }
+    if (errorCode == 1) {
+        deleteHashTable(&table);
+        return 1;
+    }
+
     printHashTable(table);
-    double test = getHashTableOccupancy(table);
 
-    double check = 0;
-    int testik = 0;
+    printf("Coefficient of occupancy in table: %g \n", getHashTableOccupancy(table));
 
-    getHashTablesListsInfo(table, &testik, &check);
+    double averageListLength = 0.0;
+    unsigned int maxListLength = 0;
+
+    getHashTablesListsInfo(table, &maxListLength, &averageListLength);
+    printf("Max length of list in hash table: %d \n", maxListLength);
+    printf("Average length of lists in hash table: %g", averageListLength);
+
+    deleteHashTable(&table);
 
     return 0;
 }
