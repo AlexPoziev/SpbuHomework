394bda13c50d08e20bead45384d482650ff9cecd
Aleksey Poziev <poziev24@gmail.com>
2022-11-14 16:59:09 +0300

    test: addd stackTest()

--- Homework5/postfixConverter/main.c
+++ Homework5/postfixConverter/main.c
@@ -1,10 +1,11 @@
+#include "../stack/stackTest.h"
 #include "postfixConverter.h"
 #include <stdio.h>
 #include <string.h>
 
 
 int main() {
-    if (!correctTest()) {
+    if (!correctTest() || !stackTest()) {
         printf("Tests Failed");
         return 1;
     }
@@ -14,14 +15,17 @@ int main() {
     printf("Input postfix expression with length below 1000: ");
     fgets(expression, sizeof(expression), stdin);
     int errorCode = postfixConverter(expression, strlen(expression) - 1);
+
     if (errorCode == -1) {
         printf("Incorrect expression");
         return 0;
     }
+
     if (errorCode == 1) {
         printf("Not enough memory");
         return 1;
     }
+
     printf("Postfix expression: %s", expression);
 
     return 0;
