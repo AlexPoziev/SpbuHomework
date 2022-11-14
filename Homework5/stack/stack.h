0cb3fc0c7d8f22ac8c720b07a94b7eed0f3928d2
Aleksey Poziev <poziev24@gmail.com>
2022-11-14 16:26:12 +0300

    refactor: add more comments

--- Homework5/stack/stack.h
+++ Homework5/stack/stack.h
@@ -15,15 +15,21 @@ typedef struct DoubleStack {
 }DoubleStack;
 
 // add value to stack
+// returns 1 if not enough memory
+// returns 0 if all is ok
 int pushInt(IntStack **head, int value);
 
 // add value to stack
+// returns 1 if not enough memory
+// returns 0 if all is ok
 int pushDouble(DoubleStack **head, double value);
 
 // delete value from stack
+// returns value of deleted value
 int popInt(IntStack **head, int *errorCode);
 
 // delete value from stack
+// returns value of deleted value
 double popDouble(DoubleStack **head, int *errorCode);
 
 // clear memory of int stack
