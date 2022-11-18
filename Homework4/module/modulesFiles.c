256949e41a584cecfa69601c7429e220777a44aa
Aleksey Poziev <poziev24@gmail.com>
2022-11-18 14:10:20 +0300

    refactor: generalize readFile function

--- Homework4/module/modulesFiles.c
+++ Homework4/module/modulesFiles.c
@@ -15,6 +15,7 @@ int mostFrequentNumber(int array[], int arrayLength, int *errorCode) {
         *errorCode = 1;
         return -1;
     }
+
     memcpy(sortedArray, array, sizeof(int) * arrayLength);
     qsortRecursion(0, arrayLength - 1, sortedArray);
 
@@ -38,9 +39,25 @@ int mostFrequentNumber(int array[], int arrayLength, int *errorCode) {
     return recentCount > maxRow ? lastNumber : maxRowElement;
 }
 
-void readFile(FILE *file, int *array, int arrayLength) {
-    for (int i = 0; i < arrayLength; ++i) {
-        fscanf(file, "%d", &array[i]);
+void readFile(FILE *file, int *array, int arrayLength, int *errorCode) {
+    int i = 0;
+    for (; i < arrayLength && !(feof(file)); ++i) {
+        int eofCheck = fscanf(file, "%d", &array[i]);
+        if (!eofCheck) {
+            *errorCode = -1;
+            return;
+        }
+
+        // EOF check
+        if (eofCheck == EOF) {
+            *errorCode = -2;
+            return;
+        }
+    }
+
+    if (i != arrayLength) {
+        *errorCode = -2;
+        return;
     }
 }
 
@@ -49,15 +66,15 @@ bool correctTest(void) {
     int errorCode = 0;
     FILE *file = fopen("test1.txt", "r");
 
-    readFile(file, firstCheck, 10);
+    readFile(file, firstCheck, 10, &errorCode);
     fclose(file);
     int secondCheck[1] = {0};
     file = fopen("test2.txt", "r");
-    readFile(file, secondCheck, 1);
+    readFile(file, secondCheck, 1, &errorCode);
     fclose(file);
     int thirdCheck[5] = {0};
     file = fopen("test3.txt", "r");
-    readFile(file, thirdCheck, 10);
+    readFile(file, thirdCheck, 10, &errorCode);
     fclose(file);
 
     return mostFrequentNumber(firstCheck, 10, &errorCode) == -6
@@ -72,6 +89,7 @@ int main() {
         printf("Test Failed");
         return 1;
     }
+
     FILE *file = fopen("data.txt", "r");
     if (file == NULL) {
         printf("File not found");
@@ -105,30 +123,30 @@ int main() {
         return 1;
     }
 
-    int i = 0;
-    for (; i < arrayLength && !(feof(file)); ++i) {
-        if (!(fscanf(file, "%d", &array[i]))) {
-            printf("Not a number");
-            free(array);
-            fclose(file);
-
-            return 2;
-        }
-    }
-    if (i != arrayLength) {
-        printf("Not enough numbers");
+    int errorCode = 0;
+    readFile(file, array, arrayLength, &errorCode);
+    if (errorCode == -1) {
+        printf("File contains not a number");
         free(array);
         fclose(file);
 
-        return 2;
+        return -1;
+    }
+    if (errorCode == -2) {
+        printf("The written number of elements less than their actual number");
+        free(array);
+        fclose(file);
+
+        return -2;
     }
 
     fclose(file);
 
-    int errorCode = 0;
     int mostFrequent = mostFrequentNumber(array, arrayLength, &errorCode);
     if (errorCode == 1) {
         printf("Not enough memory");
+        free(array);
+
         return 1;
     }
 
