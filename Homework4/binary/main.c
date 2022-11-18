ffe1eb3602cb200516b1c279c0a8dd063a424eb6
Aleksey Poziev <poziev24@gmail.com>
2022-11-19 01:18:34 +0300

    fix: another implementation

--- Homework4/binary/main.c
+++ Homework4/binary/main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
 #include <stdbool.h>
 #include <locale.h>
+
+#define NUMBER_SIZE sizeof(int) * 8
 
 void printArray(short *array, unsigned int size) {
     for (int i = 0; i < size; ++i) {
@@ -22,7 +22,6 @@ void columnAddition(const short *first, const short *second, unsigned numberDigi
 }
 
 void binaryRepresentation(int number, unsigned int numberDigits, short* representation) {
-    number = number > 0 ? number : (int)pow(2, numberDigits) + number;
 
     for (int i = (int)numberDigits - 1; i >= 0; --i) {
         representation[i] = (short)(number & 1);
@@ -101,6 +100,8 @@ int main() {
         return 1;
     }
 
+    const unsigned int numberSize = sizeof(int) * 8;
+
     printf("Введите первое число: ");
     int first = 0;
     bool isString = scanf("%d", (&first));
@@ -119,54 +120,25 @@ int main() {
         isString = scanf("%d", &second);
     }
 
-    // определение количества разрядов
-    unsigned int numberDigits = (unsigned int)ceil(log2(abs(first) > abs(second) ? abs(first) : abs(second))) + 1;
-    // для предотвращения превращения положительных чисел в отрицательные
-    if (abs(first + second) >= (pow(2, numberDigits - 1))) {
-        ++numberDigits;
-    }
 
-    short* firstBinary = (short*)(calloc(numberDigits, sizeof(short)));
-    if (firstBinary == NULL) {
-        printf("Недостаточно памяти");
-        return 1;
-    }
-    binaryRepresentation(first, numberDigits, firstBinary);
+    short firstBinary[NUMBER_SIZE] = {0};
+    binaryRepresentation(first, NUMBER_SIZE, firstBinary);
     printf("Двоичное представление в дополнительном коде первого числа: ");
-    printArray(firstBinary, numberDigits);
+    printArray(firstBinary, NUMBER_SIZE);
 
-    short* secondBinary = (short*)(calloc(numberDigits, sizeof(short)));
-    if (secondBinary == NULL) {
-        printf("Недостаточно памяти");
-        free(firstBinary);
-
-        return 1;
-    }
-    binaryRepresentation(second, numberDigits, secondBinary);
+    short secondBinary[NUMBER_SIZE] = {0};
+    binaryRepresentation(second, NUMBER_SIZE, secondBinary);
     printf("Двоичное представление в дополнительном коде второго числа: ");
-    printArray(secondBinary, numberDigits);
+    printArray(secondBinary, NUMBER_SIZE);
 
-    short *sum = (short*)(calloc(numberDigits, sizeof(short)));
-    if (sum == NULL) {
-        printf("Недостаточно памяти");
-        free(firstBinary);
-        free(secondBinary);
-
-        return 1;
-    }
-
-    columnAddition(firstBinary, secondBinary, numberDigits, sum);
-
-    free(firstBinary);
-    free(secondBinary);
+    short sum[NUMBER_SIZE] = {0};
+    columnAddition(firstBinary, secondBinary, NUMBER_SIZE, sum);
 
     printf("Сумма в двоичном представлении: ");
-    printArray(sum, numberDigits);
+    printArray(sum, NUMBER_SIZE);
 
-    decimalRepresentation(numberDigits, sum);
-    printf("Сумма в десятичном представлении: %d", decimalRepresentation(numberDigits, sum));
-
-    free(sum);
+    decimalRepresentation(NUMBER_SIZE, sum);
+    printf("Сумма в десятичном представлении: %d", decimalRepresentation(NUMBER_SIZE, sum));
 
     return 0;
 }
