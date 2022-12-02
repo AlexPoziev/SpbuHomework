691f338bc4122abc0f376f589ab3165afe579fb8
Aleksey Poziev <poziev24@gmail.com>
2022-12-03 00:42:26 +0300

    test: test for only function

--- Homework10/GraphViz/graph.c
+++ Homework10/GraphViz/graph.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+#include <sysdir.h>
+#include <stdbool.h>
 
 // mutate matrix from .txt file to .dot type code
 // return -1 if adjacent matrix isn't square
 // return -2 if no file with this name
 // return 0 if all is ok
+// return 1 if not enough memory
 int getMatrixToDotFile(char *fileName) {
     FILE *graphFile = fopen("../graph.dot", "w");
+    if (graphFile == NULL) {
+        return 1;
+    }
     FILE *matrixFile = fopen(fileName, "r");
     if (matrixFile == NULL) {
         fclose(graphFile);
@@ -51,19 +58,73 @@ int getMatrixToDotFile(char *fileName) {
     return currentRow == maxRow ? 0 : -1;
 }
 
+bool getMatrixToDotFileTest(void) {
+    FILE *file = fopen("test.txt", "w");
+    if (file == NULL) {
+        return false;
+    }
+
+    fprintf(file, "0 1 1"
+                  "\n1 0 0"
+                  "\n0 1 0");
+    fclose(file);
+
+    getMatrixToDotFile("test.txt");
+
+    file = fopen("../graph.dot", "r");
+    char test[20] = {0};
+    fscanf(file, "%[^\n]", test);
+    fgetc(file);
+    bool firstTest = !strcmp(test, "digraph Matrix {");
+    fscanf(file, "%[^\n]", test);
+    fgetc(file);
+    bool secondtest = !strcmp(test, "\t1 -> 2;");
+    fscanf(file, "%[^\n]", test);
+    fgetc(file);
+    bool thirdTest = !strcmp(test, "\t1 -> 3;");
+    fscanf(file, "%[^\n]", test);
+    fgetc(file);
+    bool fourthTest = !strcmp(test, "\t2 -> 1;");
+    fscanf(file, "%[^\n]", test);
+    fgetc(file);
+    bool fifthTest = !strcmp(test, "\t3 -> 2;");
+    fscanf(file, "%[^\n]", test);
+    bool sixthTest = !strcmp(test, "}");
+
+    remove("test.txt");
+    remove("../graph.dot");
+
+    return firstTest && secondtest && thirdTest
+            && fourthTest && fifthTest && sixthTest;
+}
+
 int main(int argc, char** argv) {
+    if (argc == 3 && !strcmp(argv[2], "--test")) {
+        if (!getMatrixToDotFileTest()) {
+            printf("Tests Failed");
+            return 1;
+        }
+
+        return 0;
+    }
+
     if (argc == 1) {
+        printf("No file input");
         return 1;
     }
 
     int errorCode = getMatrixToDotFile(argv[1]);
+    if (errorCode == 1) {
+        printf("Not enough memory");
+        return 1;
+    }
     if (errorCode == -1) {
         printf("Adjacent matrix isn't square");
-        return 1;
+        return -1;
     }
     if (errorCode == -2) {
         printf("No file with this name");
-        return -1;
+        return -2;
     }
 
     return 0;
