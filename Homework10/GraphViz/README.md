8571659b10ae5ff57f0a640e1b2f733d4384f7f6
Aleksey Poziev <poziev24@gmail.com>
2022-12-07 00:18:36 +0300

    fix: cases of spaces after rows not not error
    refactor: add switch case construction for faster work
    
    style: add \t to README.md

--- Homework10/GraphViz/README.md
+++ Homework10/GraphViz/README.md
@@ -1,8 +1,8 @@
 HOW TO WORK WITH IT:
 
-						0 1 0
+						                    0 1 0
 1) Fill matrix.txt file with matrix like: 	0 0 1
-						1 0 0
+						                    1 0 0
 
 2) Put to terminal: 
 chmod +x dotter.sh
--- Homework10/GraphViz/graph.c
+++ Homework10/GraphViz/graph.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-#include <sysdir.h>
 #include <stdbool.h>
 
 // mutate matrix from .txt file to .dot type code
@@ -8,11 +7,13 @@
 // return -2 if no file with this name
 // return 0 if all is ok
 // return 1 if not enough memory
+// return 2 if way distance less than zero
 int getMatrixToDotFile(char *fileName) {
     FILE *graphFile = fopen("../graph.dot", "w");
     if (graphFile == NULL) {
         return 1;
     }
+
     FILE *matrixFile = fopen(fileName, "r");
     if (matrixFile == NULL) {
         fclose(graphFile);
@@ -29,25 +30,38 @@ int getMatrixToDotFile(char *fileName) {
         unsigned int way = 0;
         int eofCheck = fscanf(matrixFile, "%u", &way);
         if (eofCheck == EOF) {
+            fprintf(graphFile, "}");
             fclose(graphFile);
             fclose(matrixFile);
-            fprintf(graphFile, "}");
 
-            return currentRow == maxRow ? 0 : -1;
+            return (currentRow == maxRow && currentColumn == maxRow + 1) ? 0 : -1;
+        }
+        if (eofCheck == 0) {
+            fclose(graphFile);
+            fclose(matrixFile);
 
+            return 2;
         }
 
         if (way != 0) {
-            fprintf(graphFile, "\t%u -> %u;\n", currentRow, currentColumn);
+            fprintf(graphFile, "\t%u -> %u [label=%u];\n", currentRow, currentColumn, way);
             maxRow = maxRow > currentColumn ? maxRow : currentColumn;
         }
 
-        ++currentColumn;
+        eofCheck = fgetc(matrixFile);
 
-        if (fgetc(matrixFile) == '\n') {
+        if (eofCheck == '\n') {
             ++currentRow;
-            currentColumn = 1;
+            currentColumn = 0;
+        } else if (eofCheck == EOF) {
+            fprintf(graphFile, "}");
+            fclose(graphFile);
+            fclose(matrixFile);
+
+            return (currentRow == maxRow && currentColumn == maxRow) ? 0 : -1;
         }
+
+        ++currentColumn;
     }
 
     fprintf(graphFile, "}");
@@ -78,16 +92,16 @@ bool getMatrixToDotFileTest(void) {
     bool firstTest = !strcmp(test, "digraph Matrix {");
     fscanf(file, "%[^\n]", test);
     fgetc(file);
-    bool secondTest = !strcmp(test, "\t1 -> 2;");
+    bool secondTest = !strcmp(test, "\t1 -> 2 [label=1];");
     fscanf(file, "%[^\n]", test);
     fgetc(file);
-    bool thirdTest = !strcmp(test, "\t1 -> 3;");
+    bool thirdTest = !strcmp(test, "\t1 -> 3 [label=1];");
     fscanf(file, "%[^\n]", test);
     fgetc(file);
-    bool fourthTest = !strcmp(test, "\t2 -> 1;");
+    bool fourthTest = !strcmp(test, "\t2 -> 1 [label=1];");
     fscanf(file, "%[^\n]", test);
     fgetc(file);
-    bool fifthTest = !strcmp(test, "\t3 -> 2;");
+    bool fifthTest = !strcmp(test, "\t3 -> 2 [label=1];");
     fscanf(file, "%[^\n]", test);
     bool sixthTest = !strcmp(test, "}");
 
@@ -114,18 +128,25 @@ int main(int argc, char** argv) {
     }
 
     int errorCode = getMatrixToDotFile(argv[1]);
-    if (errorCode == 1) {
-        printf("Not enough memory");
-        return 1;
+    switch (errorCode) {
+        case 1: {
+            printf("Not enough memory");
+            return 1;
+        }
+        case -1: {
+            printf("Adjacent matrix isn't square");
+            return 0;
+        }
+        case -2: {
+            printf("No file with this name");
+            return 0;
+        }
+        case 2: {
+            printf("Way distance less than zero");
+            return 0;
+        }
+        default: {
+            return 0;
+        }
     }
-    if (errorCode == -1) {
-        printf("Adjacent matrix isn't square");
-        return -1;
-    }
-    if (errorCode == -2) {
-        printf("No file with this name");
-        return -2;
-    }
-
-    return 0;
 }
